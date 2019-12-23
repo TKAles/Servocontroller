d5ea4cbdd88a72e12808a4bb9bd9e07c112b4fd6
Thomas Ales <tkales@iastate.edu>
2019-12-22 21:52:08 -0600

    Pitch slider and pitch stop working on left rear

--- __main__.py
+++ __main__.py
@@ -14,17 +14,26 @@ import servocontroller
 class Ui(QMainWindow):
 
     def __init__(self):
+        # Load the ui file from the same directory
         super(Ui, self).__init__()
         loadUi('servomcu_ui.ui', self)
+        # Initalize the ServoController object, and populate the COM port list.
         self.s_controller = servocontroller.ServoController()
+
         for comport in self.s_controller.list_coms():
             self.comport_combo.addItem(comport)
         
+        # Qt Signals/Slots routing
         self.mcu_toggleconnect.clicked.connect(partial(self.mcu_toggle, self.comport_combo.currentText()))
-
+        self.lr_pitch_slider.valueChanged.connect(self.lf_pitch_update)
+        self.lr_pitch_stop.clicked.connect(self.lf_pstop)
+        # Show the UI
         self.show()
 
     def mcu_toggle(self, comport):
+        
+        """mcu_toggle(comport): Connect/Disconnect to the microcontroller and update the MCU connect/disconnect button."""
+
         if(self.mcu_toggleconnect.text() == "Connect"):
             self.s_controller.connect_mcu(comport)
             self.mcu_toggleconnect.setText("Disconnect")
@@ -32,10 +41,16 @@ class Ui(QMainWindow):
             self.s_controller.disconnect_mcu()
             self.mcu_toggleconnect.setText("Connect")
 
+    def lf_pitch_update(self, angle):
+        self.s_controller.update_servo(self.s_controller.servo_pins[0], angle)
+
+    def lf_pstop(self):
+        self.lr_pitch_slider.setValue(self.s_controller.servo_stop_position)
+
 if __name__ == '__main__':
+
     import sys
     import PyQt5
     app = QApplication(sys.argv)
     window = Ui()
-
     sys.exit(app.exec_())
--- servocontroller.py
+++ servocontroller.py
@@ -8,18 +8,39 @@ class ServoController:
         self.com_port = ''
         self.mcu_instance = ''
         self.mcu_iterator = ''
+        self.servo_stop_position = 90
+        self.servo_min_pulse_us = 400
+        self.servo_max_pulse_us = 2400
+
+        self.micro_configuration = {"LeftRear": {"Pitch": 5, "Yaw": 6},
+                                    "RightRear": {"Pitch": 7, "Yaw": 8},
+                                    "LeftFront": {"Pitch": 14, "Yaw": 15},
+                                    "RightFront": {"Pitch": 16, "Yaw": 17},
+                                    "InsertTools": {"Left": 18, "Right": 19},
+                                    "Prism": 20}
 
     def connect_mcu(self, com_port):
+        """connect_mcu(com_port): Attempts to connect to a microcontroller running StandardFirmata on com_port."""
+
         self.mcu_instance = pyfirmata.ArduinoMega(com_port)
         self.mcu_iterator = pyfirmata.util.Iterator(self.mcu_instance)
         self.mcu_iterator.start()
+        self.attach_servos()
 
     def disconnect_mcu(self):
+        """disconnect_mcu(): Cleanly shutsdown the instance of the microcontroller associated with this object."""
         self.mcu_instance.exit()
         
     def list_coms(self):
+        """list_coms(): returns a list of the available COM ports on Windows platforms."""
         com_strings = []
         for comports in serial.tools.list_ports.comports():
             com_strings.append(comports.device)
-
         return com_strings
+    
+    def attach_servos(self):
+        self.servo_pins = [self.mcu_instance.get_pin('d:'+str(self.micro_configuration["LeftRear"]["Pitch"])+':s')]
+        self.servo_pins[0].write(self.servo_stop_position)
+
+    def update_servo(self, servo_pin, angle):
+        servo_pin.write(int(angle))
